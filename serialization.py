81dbea55e3d28f76c388fb74d2dff609c49dab2d
kozenumezawa <umezawa@viz.media.kyoto-u.ac.jp>
2017-09-04 19:37:05 +0900

    Update: to serialize

--- serialization.py
+++ serialization.py
@@ -7,11 +7,23 @@ if __name__ == '__main__':
     for time in ['000', '005', '010', '015', '020', '025', '030', '035', '040', '045', '050']:
         name = './Nagumo/Div50_' + time + '.0000.csv'
         f = open(name, 'r')
-        dataReader = csv.reader(f)
+        data_reader = csv.reader(f)
 
-        
-        for row in dataReader:
-            print row
-        data = [['lat', 'lon', 'value']]
-        writer.writerows(data)
+        serial_data = []
+        for row in data_reader:
+            serial_data.extend(row)
+        writer.writerows([serial_data])
 
+
+    f = open('./data/SD.csv', 'w')
+    writer = csv.writer(f)
+
+    for time in ['000', '005', '010', '015', '020', '025', '030', '035', '040', '045', '050']:
+        name = './SD/Div50_' + time + '.0000.csv'
+        f = open(name, 'r')
+        data_reader = csv.reader(f)
+
+        serial_data = []
+        for row in data_reader:
+            serial_data.extend(row)
+        writer.writerows([serial_data])
